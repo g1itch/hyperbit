7e8b8c1061dbc6c95ca4d3d64bdd8099701fc749
Dmitri Bogomolov <4glitch@gmail.com>
2021-07-13 16:22:18 +0300

    Start without GUI if cannot import PyQt5 or qasync

--- hyperbit/main.py
+++ hyperbit/main.py
@@ -1,29 +1,35 @@
-# Copyright 2015-2016 HyperBit developers
+# Copyright 2015-2021 HyperBit developers
 
 import asyncio
 import sys
 
 import _cffi_backend  # noqa:F401 for PyInstaller
-import qasync
-from PyQt5 import QtWidgets
+try:
+    import qasync
+    from PyQt5.QtWidgets import QApplication
+except ImportError:
+    gui = None
+else:
+    from hyperbit.gui import gui
 
 from hyperbit import core2
-from hyperbit.gui import gui
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    asyncio.set_event_loop(qasync.QEventLoop(app))
+    if gui:
+        app = QApplication(sys.argv)
+        asyncio.set_event_loop(qasync.QEventLoop(app))
 
     @asyncio.coroutine
     def run():
         core = core2.Core()
-        window = gui.MainWindow(core)
-        if not core.get_config('network.proxy'):
-            if not window.configure_network():
-                return
+        if gui:
+            window = gui.MainWindow(core)
+            if not core.get_config('network.proxy'):
+                if not window.configure_network():
+                    return
+            window.show()
         asyncio.get_event_loop().create_task(core.run())
-        window.show()
 
     asyncio.get_event_loop().create_task(run())
     asyncio.get_event_loop().run_forever()
