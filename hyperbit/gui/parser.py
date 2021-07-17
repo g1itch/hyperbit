7959505b1038fd1cd6040b1f2c56093e7f7017f0
Dmitri Bogomolov <4glitch@gmail.com>
2021-07-17 23:00:06 +0300

    Experimental HTML mode on MessagesTab.messages:
    implemented in parser, a separate module with try on lxml import

---
+++ hyperbit/gui/parser.py
@@ -0,0 +1,72 @@
+# Copyright 2021 HyperBit developers
+
+from html.parser import HTMLParser
+
+try:
+    import lxml.etree
+    from lxml.html import clean
+except ImportError:
+    clean = None
+
+
+class SimpleHtmlParser(HTMLParser):
+    """This is solely for HTML detection."""
+
+    has_html = False
+    nonstd = False
+    allow_tags = (
+        'a', 'abbr', 'acronym', 'address', 'area', 'article', 'aside',
+        'audio', 'b', 'big', 'blockquote', 'br', 'button',
+        'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup',
+        'command', 'datagrid', 'datalist', 'dd', 'del', 'details', 'dfn',
+        'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'event-source', 'fieldset',
+        'figcaption', 'figure', 'footer', 'font', 'header', 'h1',
+        'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'ins',
+        'keygen', 'kbd', 'label', 'legend', 'li', 'm', 'map', 'menu', 'meter',
+        'multicol', 'nav', 'nextid', 'ol', 'output', 'optgroup', 'option',
+        'p', 'pre', 'progress', 'q', 's', 'samp', 'section', 'select',
+        'small', 'sound', 'source', 'spacer', 'span', 'strike', 'strong',
+        'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'time', 'tfoot',
+        'th', 'thead', 'tr', 'tt', 'u', 'ul', 'var'
+    )
+
+    def handle_starttag(self, tag, attrs):
+        if not self.has_html:
+            if tag in ('img', 'video', 'a') or tag in self.allow_tags:
+                self.has_html = True
+
+
+class MessageCleaner():
+    def __init__(self):
+        self.cleaner = clean.Cleaner(
+            links=False,
+            # remove_tags=('img',),
+            safe_attrs_only=True,
+            # remove_unknown_tags=False,
+            allow_tags=SimpleHtmlParser.allow_tags
+        ) if clean else None
+        self.html = False
+
+    def clean_html(self, text):
+        self.html = False
+        if not self.cleaner:
+            return text
+
+        try:
+            html = lxml.html.fromstring(
+                text, parser=lxml.html.HTMLParser(recover=False))
+        except lxml.etree.XMLSyntaxError:
+            html = ''
+
+        if not len(html):
+            parser = SimpleHtmlParser()
+            parser.feed(text)
+            html = ' ' if parser.has_html else ''
+            if not parser.has_html:
+                return text
+            else:
+                try:
+                    self.html = True
+                    return self.cleaner.clean_html(text)
+                except lxml.etree.ParserError:
+                    pass
