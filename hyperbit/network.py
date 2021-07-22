9d99866b80f05ffc2bf6f73f7bfb5b7cf9704076
Dmitri Bogomolov <4glitch@gmail.com>
2021-07-22 22:43:04 +0300

    Handle OSError in network.Connection2.run()

--- hyperbit/network.py
+++ hyperbit/network.py
@@ -425,7 +425,18 @@ class Connection2():
                 elif generic.command == 'object':
                     obj = packet.Object.from_bytes(generic.data)
                     self.om.add_object(obj)
-            generic = yield from self._c.recv_packet()
+                elif generic.command == 'ping':
+                    self._c.send_packet(packet.Generic('pong', ''))
+                else:
+                    logger.warning('unsuitable command %s', generic.command)
+
+            # FIXME: there is a possibility for unknown command
+            # before fully_established
+            try:
+                generic = yield from self._c.recv_packet()
+            except OSError:
+                logger.warning('OSError in connection loop', exc_info=True)
+                break
 
         self.set_disconnected()
 
